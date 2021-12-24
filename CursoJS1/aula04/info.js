15db04e9130298c75bc49f1bccae0e3b85e9cd5f
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2021-12-24 12:27:10 -0300

    update js

---
+++ CursoJS1/aula04/info.js
@@ -0,0 +1,4 @@
+var vaga = carro //vaga recebe(=) carro
+window.alert('Minha primeira mensagem!') //alerta uma mensagem//
+window.confirm('Está gostando de JS?')//cria uma confirmaçao com cancelamento//
+window.prompt('Qual é o seu nome?')//cria um campo de digitaçao com pergunta//
