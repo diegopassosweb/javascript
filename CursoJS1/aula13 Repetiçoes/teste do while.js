9c09c3838982c7bdb51f1fed71bb22c5614d084d
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2021-12-30 01:38:17 -0300

    update do while

---
+++ CursoJS1/aula13 Repetiçoes/teste do while.js
@@ -0,0 +1,25 @@
+//EXECUTA PRIMEIRO E TESTA DEPOIS
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+/* TESTA PRIMEIRO E EXECUTA DEPOIS
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+*/
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
