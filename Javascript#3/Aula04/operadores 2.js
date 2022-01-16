cba0824369a6053ef309dc776414c2e6d925c52f
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-01-16 05:35:31 -0300

    Create operadores 2.js

---
+++ Javascript#3/Aula04/operadores 2.js
@@ -0,0 +1,45 @@
+//Relacionais
+/*
+
+> ---> maior
+< ---> menor
+>= ---> maior igual
+<= ---> menor igual
+== ---> igual
+!= ---> não igual
+
+*/
+5 > 2 --> true
+7 < 4 --> false
+8 >= 8--> true
+8 <= 7 --> false
+5 == 5 --> true
+4 != 4 --> false
+5 == '5' --> true
+5 === '5' --> false
+// Logicos
+/*
+
+! ---> negaçao
+&& ---> conjunçao E
+|| ---> disjunçao OU
+
+*/
+
+//Primeiro
+!true --> false
+!false --> true
+//Segundo
+true && true --> true
+true && false --> false
+false && false --> false
+false && true --> false
+//Terceiro
+true || true --> true
+true || false --> true
+false || true --> true
+false || false -->false
+//Ternario
+teste ? true : false 
+media >= 7.0 ? "Aprovado" : "Reprovado"
+
