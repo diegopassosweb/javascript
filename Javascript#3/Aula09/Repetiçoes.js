09089fd76b31a08bbace876e075073ea9fe90636
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-01-19 15:37:38 -0300

    update js

---
+++ Javascript#3/Aula09/Repetiçoes.js
@@ -0,0 +1,13 @@
+console.log('Vai começar...')
+for(var c =1; c<= 5 ;c++){
+    console.log(`Passo ${c}`)
+}
+console.log('Fim!')
+
+/*
+var c = 1
+while (c <= 5) {
+    console.log('tudo bem')
+    c++
+}
+*/
