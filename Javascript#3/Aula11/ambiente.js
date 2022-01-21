7d29387b7f5bf90db3cb28b19be8f8cf5ce74df8
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-01-21 09:43:13 -0300

    update js

--- Javascript#3/Aula11/ambiente.js
+++ Javascript#3/Aula11/ambiente.js
@@ -11,4 +11,8 @@ console.log(num)
 console.log(`Nosso vetor tem ${num.length} posiçoes`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 let pos = num.indexOf(8)
+if (pos == -1) {
+    console.log('O valor nao foi encontrado!')
+} else {
 console.log(`O valor 8 esta na posiçao ${pos}`)
+}
