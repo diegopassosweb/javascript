f8bc7688a97e37d74e4981daef21fa6990a35fef
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-01-21 09:41:39 -0300

    update js

--- Javascript#3/Aula11/ambiente.js
+++ Javascript#3/Aula11/ambiente.js
@@ -5,7 +5,10 @@ num.push(7) // adiciona o valor 7 na ultima posição
 num.length // comprimento do vetor
 num.sort() // organiza a primeira casa decimal em ordem crescente
 */
+num.push(1)
 num.sort()
 console.log(num)
 console.log(`Nosso vetor tem ${num.length} posiçoes`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+console.log(`O valor 8 esta na posiçao ${pos}`)
