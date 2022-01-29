c3f5afc1f7e95f999a602d78bcbd400d8dd962e3
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-01-29 14:46:07 -0300

    update js

---
+++ Javascript#3/Aula11/ambients.js
@@ -0,0 +1,6 @@
+let num = [5,8,2,9,3]
+num[3] = 6 //coloque o valor 6 na posiçao 3
+num.push(7)//acrescente um valor a ultima posicáo
+num.length//comprimento, quantas letras
+num.sort()//organiza de acordo com a primeira casa decimal
+console.log(`Nosso vetor é o ${num}`)
---
+++ Javascript#3/Aula11/for.js
@@ -0,0 +1,14 @@
+let valores = [8,1,7,4,2,9]
+
+for(let pos in valores) { // para cada POS em VALORES
+    console.log(`A posiçao ${pos} tem o valor ${valores[pos]}`) 
+}
+
+/*
+for(let pos=0; pos<valores.length;pos++) {
+    console.log(`A posiçao ${pos} tem o valor ${valores[pos]}`)
+}
+valores.indexOf(7) - busca o valor 7 e mostra o vetor
+//console.log(valores[0])
+//console.log(valores)
+*/
