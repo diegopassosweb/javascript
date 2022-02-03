f8af4a38c432fa2a939b0ba5eb5b405ca474b9e8
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-02-03 01:48:05 -0300

    update js

---
+++ Javascript#3/Bonus/022/For-off.js
@@ -0,0 +1,27 @@
+// for-of
+
+// for 1mais usado
+// while 2mais usado
+// do-while
+
+//for-in
+
+const pessoa = {
+    nome: 'jhonatan',
+    
+};
+//key-value
+for(let chave in pessoa){
+    console.log(chave,pessoa['nome']);
+}
+
+const cores = ['vermelho', 'azul', 'verde'];
+
+for(let indice in cores){
+    console.log(indice,cores[indice]);
+}
+
+//for-of
+for(let cor of cores){
+    console.log(cor);
+}
---
+++ Javascript#3/Bonus/ex023/maximo entre 2 valores.js
@@ -0,0 +1,11 @@
+// Escreva uma funçao que use 2 numeros e retorna o maior entre eles
+
+let maior = max(9,14);
+console.log(maior);
+function max(n1,n2){
+    // if( n1 > n2 )
+    //     return n1;
+    
+    //     return n2;
+    return n1 > n2 ? n1:n2 ;
+}
