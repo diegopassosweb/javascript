0eb3ca3fbaa6f28a362570d83746dee844ed4cf4
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-02-04 12:10:21 -0300

    Create Contador Asteriscos.js

---
+++ Javascript#3/Bonus/ex030/Contador Asteriscos.js
@@ -0,0 +1,23 @@
+// Cria uma funçào que exibe a quantidade de *
+// que aquela linha possui
+
+exibirAsteriscos(5);
+function exibirAsteriscos(linhas){
+
+    // Loop interno, nestado
+    // for(let linha =1; linha <= linhas; linha++){
+    //     let padrao = '';
+    //     for(let i = 0; i < linha; i++){
+    //         padrao += '*';
+    //     }
+    //     console.log(padrao);
+
+    let padrao = '';
+    for(let linha = 1; linha <= linhas; linha++){
+        padrao += '*';
+        console.log(padrao);
+        }
+    }
+
+
+    
