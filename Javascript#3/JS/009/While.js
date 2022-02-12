423b10b3b604fe5ef7dc230df6bbc9380d496186
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-02-12 15:08:44 -0300

    update js

---
+++ Javascript#3/JS/009/While.js
@@ -0,0 +1,20 @@
+/*
+console.log('Repetiçao')
+console.log('Repetiçao')
+console.log('Repetiçao')
+console.log('Repetiçao')
+console.log('Repetiçao')
+*/
+var c = 1
+while (c < 6){
+    console.log(`Repetiçao ${c}`)
+    c++
+}
+
+// ou
+
+var c = 1
+do {
+    console.log(`Repetiçao`)
+    c++
+} while (c <= 6)
