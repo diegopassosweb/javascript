d4fc470f3dc26ecb33b53096e3c2c2d05c0ff575
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-02-16 05:30:47 -0300

    update js

---
+++ Javascript#3/JS/013/Pos2.js
@@ -0,0 +1,9 @@
+ let letras = ['X', 'H', 'O', 'P']
+// for(let pos =0; pos < letras.length; pos++)
+
+// console.log(`A posiçao ${pos} tem as letras ${letras[pos]}`)
+letras.sort()
+
+for(let pos in letras){
+    console.log(`A posiçao ${pos} tem as letras ${letras[pos]}`)
+}
---
+++ Javascript#3/JS/013/varComposta2.js
@@ -0,0 +1,11 @@
+var num = ['A','B','C'] // <-- tipo Array vetor
+//começa com indice 0
+num[3] = 'D' // adiciona o 'D' na osiçao 3
+num.push('F') // adiciona 'F' a ultima posiçao
+num.length // quantidade de elementos
+num.sort() // ordena de acordo com a primeira casa decimal
+
+
+console.log(num)
+console.log(`O vetor tem ${num.length} posiçoes`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
