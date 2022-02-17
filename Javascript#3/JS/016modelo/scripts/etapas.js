78288223281dae7e15c1fe9504f22b310ca56613
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-02-17 12:25:24 -0300

    update js

---
+++ Javascript#3/JS/016modelo/scripts/etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'FULANO DE TAL',
+                partido: 'ABC',
+                fotos: [
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'BELTRANO DA SILVA',
+                partido: 'DEFG',
+                fotos: [
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Clicano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.pg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                fotos: [
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.pg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
