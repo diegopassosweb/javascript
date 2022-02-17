6f083a38369d85e62f052728973673d19eea176e
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-02-17 12:25:09 -0300

    update js

--- Javascript#3/JS/014/function.js
+++ Javascript#3/JS/014/function.js
@@ -1,10 +1,15 @@
-function parimp(n){
-     if(n % 2 ==0){
-         return 'par'
-     } else {
-         return 'impar'
-     }
+// function parimp(n){
+//      if(n % 2 ==0){
+//          return 'par'
+//      } else {
+//          return 'impar'
+//      }
+// }
+// //let res = parimp(11)
+// //console.log(res)
+// console.log(parimp(11))
+
+function soma(n1=0,n2=0){
+    return n1+n2
 }
-//let res = parimp(11)
-//console.log(res)
-console.log(parimp(11))
+console.log(soma(2,5))
