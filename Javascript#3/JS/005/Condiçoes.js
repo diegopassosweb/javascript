f9e628ef56c9167b4685fcab387afd9899f5d37d
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-02-20 07:01:34 -0300

    update js

---
+++ Javascript#3/JS/005/Condiçoes.js
@@ -0,0 +1,9 @@
+let vel = 60.5
+console.log(`A velocidade é de ${vel} km/h`)
+if(vel > 60){
+    console.log('Voce ultrapassou a velocidade permitida, MULTADO!')
+}
+console.log('Dirija sempre com cinto de segurança!')
+
+
+console.log('Escrevendo no node js')
