1103ba0f4560ab92325b4da975bcc0cc2674cde5
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-03-01 08:57:06 -0300

    update js

---
+++ JavaScript#4/Condiçoes/Composta.js
@@ -0,0 +1,7 @@
+let pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil' || pais == 'brasil') {
+    console.log(' Voce é Brasileiro!')
+} else {
+    console.log(' Voce é Estrangeiro!')
+}
