4f6d4515406511c9f0957573db31b65d9d124ef0
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-03-09 10:50:44 -0300

    update js

---
+++ JavaScript#4/JS/001/script.js
@@ -0,0 +1,12 @@
+let n1 = 47;
+let n2 = 5;
+
+let total = n1 + n2;
+
+console.log('O total foi: ' + total);
+
+if(total > 50) {
+    console.log('Total maior que 50');
+} else {
+    console.log('total menor que 50')
+}
