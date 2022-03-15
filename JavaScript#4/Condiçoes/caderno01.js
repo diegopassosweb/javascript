6909ef6974425a8790ba7e568add0d36eb2c4e55
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-03-15 11:11:47 -0300

    update js

---
+++ JavaScript#4/Condiçoes/caderno01.js
@@ -0,0 +1,10 @@
+console.log('O console funcionou corretamente!')
+
+let vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60) {
+    console.log('Voce ultrapassou a velocidade permitida, MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
+
+//condiçaoo simples
