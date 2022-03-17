e4a694c9ce947fd4a7f4c9d825982b04557963a8
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-03-17 15:58:01 -0300

    update js

---
+++ JavaScript#4/Repetiçoes1/caderno01.js
@@ -0,0 +1,20 @@
+let c = 1
+while(c <= 6) {
+    console.log('tudo bem?')
+    c++
+}
+
+// let c = 1
+// do {
+//     console.log('tudo bem?')
+//     c++
+// }while(c <= 6)
+
+
+
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
---
+++ JavaScript#4/Repetiçoes1/caderno02.js
@@ -0,0 +1,9 @@
+for(let c=1; c<=10; c++){
+    console.log('tudo bem?')
+}
+
+// let c = 1
+// while(c <= 5){
+//     console.log('tudo bem?')
+//     c++
+// }
