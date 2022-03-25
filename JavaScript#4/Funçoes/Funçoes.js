c71c7b3f390d8fca3a656afa486f44235a18a410
Diego dos Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-03-25 12:42:19 -0300

    update js

---
+++ JavaScript#4/Funçoes/Funçoes.js
@@ -0,0 +1,16 @@
+
+function soma(a,b) { //funcáo simples
+   return a+b
+ }
+// console.log(soma(7,8))
+
+
+let soma = function (a,b) { // funçao anonima
+     return a + b
+ }
+// console.log(soma(7,8))
+
+
+let soma = (a,b) => a+b //funçao arrow
+
+console.log(soma(7,8))
---
+++ JavaScript#5/Funções/function01.js
@@ -0,0 +1,15 @@
+//COM UM PARAMETRO
+
+function gritar(expr) {
+    return expr.toUpperCase() + '!!!!!'
+}
+const x = gritar('bom dia')
+console.log(x)
+
+//Exemplo 2
+
+function somar(a,b) {
+    return a + b
+}
+const resultado1 = somar(3,4)
+console.log(resultado1)
